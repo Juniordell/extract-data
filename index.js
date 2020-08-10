a572cb230a44893e3fdf86fb0377ffa8b9063b7a
Nelson Jr <juniordell123@gmail.com>
2020-08-10 19:28:33 -0300

    Preparing the data

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.instagram.com/rocketseat_oficial');
-  await page.evaluate(() => {
+
+  const imgList = await page.evaluate(() => {
 
     const nodeList = document.querySelectorAll('article img')
 
     const imgArray = [ ...nodeList ]
 
-    const list = imgArray.map(({src}) => ({
+    const imgList = imgArray.map(({src}) => ({
         src
     }))
 
-    console.log(list)
+    return imgList
 
   });
 
-//   await browser.close();
+  fs.writeFile('instagram.json', JSON.stringify(imgList, null, 2), err => {
+      if (err) throw new Error('something went wrong')
+
+      console.log('well done!')
+  })
+
+  await browser.close();
 })();
